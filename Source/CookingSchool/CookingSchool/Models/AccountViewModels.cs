f13651401aff8ef9ce530aba04386944d50c9b1a
Danny Minh <danny.minh0893@gmail.com>
2017-05-12 01:48:23 +0700

    add login, logup view

--- Source/CookingSchool/CookingSchool/Models/AccountViewModels.cs
+++ Source/CookingSchool/CookingSchool/Models/AccountViewModels.cs
@@ -65,6 +65,12 @@ namespace CookingSchool.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
